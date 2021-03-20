3000a2e210b230232571927eebe7257868307c38
unknown <tayanesilvasouzas@gmail.com>
2021-03-19 21:33:26 -0300

    finalizando

--- jogo.js
+++ jogo.js
@@ -1,5 +1,21 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 10
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?','')
+
+if(nivel === 'facil'){
+    var criaMosquitoTempo = 1500
+}else if(nivel === 'medio'){
+    var criaMosquitoTempo = 1000
+}else if(nivel === 'dificil'){
+    var criaMosquitoTempo = 750
+
+}
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -10,12 +26,38 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function(){
+
+    tempo -=1
+
+    if(tempo < 0 ){
+      clearInterval(cronometro)
+      clearInterval(criaMosquito)
+      window.location.href = 'vitoria.html'
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+   
+  
+}, 1000)
 
 function posicaoRandomica(){
 
 //remover o mosquito anterior caso exista
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3){
+
+            window.location.href = 'fim_jogo.html'
+
+        }else{
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+            vidas++
+        }
+
+        
     }
     
     
@@ -39,6 +81,9 @@ mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.position = 'absolute'
 mosquito.id = 'mosquito'
+mosquito.onclick = function(){
+    this.remove()
+}
 
 document.body.appendChild(mosquito)
 
